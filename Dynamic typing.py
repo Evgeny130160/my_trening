6f92aee80fac3ca9e98528eaab7fb5c958b83a80
Evgeny130160 <sagyn1301@gmail.com>
2024-09-29 02:09:22 +0300

    Add files via upload

---
+++ Dynamic typing.py
@@ -0,0 +1,5 @@
+total_tasks = 12
+hours_spent = 1.5
+name = 'Python'
+Time_one_task= hours_spent /  total_tasks
+print( 'Well:Python, total_tasks:12, hours_spent:1.5, average execution time: 0.125')
